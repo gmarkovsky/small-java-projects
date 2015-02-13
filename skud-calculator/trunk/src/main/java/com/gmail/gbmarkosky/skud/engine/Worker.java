b57c54c01ffb65d7ce3efa4944b4646b36c104d0
gbmarkovsky <gbmarkovsky@gmail.com>
2015-02-13 22:17:13 +0000

    Переделан под работу без заголовка. Использован commons-cli

--- skud-calculator/trunk/src/main/java/com/gmail/gbmarkosky/skud/engine/Worker.java
+++ skud-calculator/trunk/src/main/java/com/gmail/gbmarkosky/skud/engine/Worker.java
@@ -6,11 +6,11 @@ import java.util.Map;
 
 public class Worker {
 	private final String name;
-	private final Map<String, String> marks = new HashMap<String, String>();
-	private final Map<String, String> marksView = Collections.unmodifiableMap(marks);
+	private final Map<Integer, String> marks = new HashMap<Integer, String>();
+	private final Map<Integer, String> marksView = Collections.unmodifiableMap(marks);
 	
-	private final Map<String, String> week = new HashMap<String, String>();
-	private final Map<String, String> weekView = Collections.unmodifiableMap(week);
+	private final Map<Integer, String> week = new HashMap<Integer, String>();
+	private final Map<Integer, String> weekView = Collections.unmodifiableMap(week);
 	
 	private String total;
 	private String line;
@@ -21,19 +21,19 @@ public class Worker {
 		this.name = name;
 	}
 	
-	public void putMark(String key, String val) {
+	public void putMark(Integer key, String val) {
 		marks.put(key, val);
 	}
 	
-	public Map<String, String> getMarks() {
+	public Map<Integer, String> getMarks() {
 		return marksView;
 	}
 
-	public void putWeek(String key, String val) {
+	public void putWeek(Integer key, String val) {
 		week.put(key, val);
 	}
 	
-	public Map<String, String> getWeeks() {
+	public Map<Integer, String> getWeeks() {
 		return weekView;
 	}
 	
@@ -73,14 +73,15 @@ public class Worker {
 		this.line3 = line3;
 	}
 	
-	public static Worker fromString(String s, String[] header) {
-		String[] split = s.split(";");
+	public static Worker fromString(String s, int daysCount, String separator) {
+		String[] split = s.split(separator);
 		
 		Worker worker = new Worker(split[0]);
 		
-		for (int i = 0; i < header.length; i++) {
-			String val = (i + 3 < split.length) ? split[i+3] : "";
-			worker.putMark(header[i], val);
+		for (int i = 1; i <= daysCount; i++) {
+			int withOffset = i + 2;
+			String val = (withOffset < split.length) ? split[withOffset] : "";
+			worker.putMark(i, val);
 		}
 		
 		return worker;
