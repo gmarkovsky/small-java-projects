d0c3641860af192a54d7c2c3f6d27e8f8610a6d8
gbmarkovsky@gmail.com <gbmarkovsky@gmail.com@e2935673-5265-be52-1b78-bb328d2597fc>
2015-02-17 22:00:13 +0000

    help option

--- skud-calculator/trunk/src/main/java/com/gmail/gbmarkosky/skud/app/Application.java
+++ skud-calculator/trunk/src/main/java/com/gmail/gbmarkosky/skud/app/Application.java
@@ -3,13 +3,37 @@ package com.gmail.gbmarkosky.skud.app;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.cli.BasicParser;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
 import com.gmail.gbmarkosky.skud.engine.Worker;
 import com.gmail.gbmarkosky.skud.io.Formatter;
 import com.gmail.gbmarkosky.skud.io.IOUtils;
 
 public class Application {
 	public static void main(String[] args) {
+		Options options = new Options();
+		Option help = new Option( "help", "Print help message" );
+		options.addOption(help);
 		CommandReader commandReader = new CommandReader();
+		CommandLineParser parser = new BasicParser();
+		try {
+			CommandLine cmd = parser.parse(options, args, true);
+			if (cmd.hasOption("help")) {
+				HelpFormatter formatter = new HelpFormatter();
+				formatter.printHelp("skud-calculator FILE [OPTIONS]", commandReader.getOptions());
+				return;
+			}
+		} catch (ParseException e) {
+
+		}
+		
+		
 		Configuration configuration;
 		try {
 			configuration = commandReader.readConfiguration(args);
@@ -17,6 +41,10 @@ public class Application {
 			return;
 		}
 
+		if (configuration.isPrintHelp()) {
+			
+		}
+		
 		List<String> lines = IOUtils.readLines(configuration.getPathToFile());
 		
 		List<String> dataLines = (configuration.isSkipHeader()) ? lines.subList(1, lines.size()): lines;
